4f0737494881a441166dff0cbf42f66e6ab0f924
X-sudo-c <x1dragonn@gmail.com>
2025-10-07 01:26:06 +0000

    added code deploy to the stage this time

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world - version 5 ",
+                "message": "hello world - version 6 ",
                 # "location": ip.text.replace("\n", "")
             }
         ),
